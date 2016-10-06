20bf31694b1145afc406b90307ef18392f96f706
Luis Rene Onate <lonate12@gmail.com>
2016-10-06 13:37:43 -0400

    Receiving ajax responses.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+var $ = require('jquery');
+var _ = require('underscore');
+var Handlebars = require('handlebars');
+var gitHubToken = require('./gitapikey.js');
+
+if(gitHubToken !== undefined){
+  $.ajaxSetup({
+    headers: {
+      'Authorization': 'token ' + gitHubToken.token
+    }
+  });
+}
+
+$.ajax('https://api.github.com/users/lonate12').then(populate);
+$.ajax('https://api.github.com/users/lonate12/repos').then(populate);
