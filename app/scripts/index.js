677990a01e03aed2c509d48d5384f7da99c47c16
Luis Rene Onate <lonate12@gmail.com>
2016-10-07 13:40:59 -0400

    Set repo nav to fixed on scroll.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -13,3 +13,21 @@ if(gitHubToken !== undefined){
 
 // $.ajax('https://api.github.com/users/lonate12').then(populate);
 // $.ajax('https://api.github.com/users/lonate12/repos').then(populate);
+
+$(window).on('scroll', changeToFixed);
+
+function changeToFixed(event){
+  event.preventDefault();
+  if($('.user-info').offset().top <= $(window).scrollTop()){
+    $('.repo-nav-container').addClass('fixed');
+    console.log('it happened');
+  }else{
+    $('.repo-nav-container').removeClass('fixed');
+  }
+
+  if(($('.large-picture').offset().top + 233) <= $(window).scrollTop()){
+    $('.extended-section-top').addClass('appear');
+  }else{
+    $('.extended-section-top').removeClass('appear');
+  }
+}
