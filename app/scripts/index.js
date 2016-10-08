753cbb47e8cd3a57d46816a6d95df539a126b98c
Luis Rene Onate <lonate12@gmail.com>
2016-10-08 17:50:43 -0400

    Took off token.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -3,13 +3,13 @@ var _ = require('underscore');
 var Handlebars = require('handlebars');
 var gitHubToken = require('./gitapikey.js');
 
-if(gitHubToken !== undefined){
-  $.ajaxSetup({
-    headers: {
-      'Authorization': 'token ' + gitHubToken.token
-    }
-  });
-}
+// if(gitHubToken !== undefined){
+//   $.ajaxSetup({
+//     headers: {
+//       'Authorization': 'token ' + gitHubToken.token
+//     }
+//   });
+// }
 
 $.ajax('https://api.github.com/users/lonate12').then(populate);
 $.ajax('https://api.github.com/users/lonate12/repos').then(createRepos);
